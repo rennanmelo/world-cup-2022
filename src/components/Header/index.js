9687d89ac7a604973238b1f7d76a126aa44efbd5
Rennan Melo <rennan.melo11@gmail.com>
2021-07-16 06:03:44 -0300

    Changed background color on window scrolling event

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,27 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AppBar, Toolbar, IconButton, makeStyles } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import SideDrawer from "../SideDrawer";
 
 import "./index.css";
 
-const useStyles = makeStyles((theme) => ({
-  header: {
-    backgroundColor: "#2f2f2f",
-    boxShadow: "none",
-    padding: "8px 0",
-    flexGrow: 1,
-  },
-}));
-
 const Header = () => {
-  const classes = useStyles();
-
   const [isOpen, setIsOpen] = useState(false);
+  const [headerTransparent, setHeaderTransparent] = useState(true);
+
+  const useStyles = makeStyles((theme) => ({
+    header: {
+      backgroundColor: headerTransparent ? "transparent" : "#2f2f2f",
+      boxShadow: "none",
+      padding: "8px 0",
+      flexGrow: 1,
+      transition: "background 0.5s",
+    },
+  }));
+
+  const classes = useStyles();
 
   const handleDrawer = (status) => {
     setIsOpen(!status);
   };
+
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setHeaderTransparent(false);
+    } else {
+      setHeaderTransparent(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <AppBar position="fixed" className={`${classes.header} header`}>
