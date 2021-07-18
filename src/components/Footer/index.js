92397e84c4d89412f59b3e6b31f303afe8e838b5
Rennan Melo <rennan.melo11@gmail.com>
2021-07-18 09:41:25 -0300

    Added fade animation on the footer's components

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { Fade } from "react-awesome-reveal";
 import "./index.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer__title">
-        <span>Qatar 2022</span>
-      </div>
-      <div className="footer__copyright">
-        <span>All rights reserved</span>
-      </div>
+      <Fade triggerOnce delay={300}>
+        <div className="footer__title">
+          <span>Qatar 2022</span>
+        </div>
+      </Fade>
+
+      <Fade triggerOnce delay={600}>
+        <div className="footer__copyright">
+          <span>All rights reserved</span>
+        </div>
+      </Fade>
     </footer>
   );
 };
