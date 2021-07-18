bf189e2908ea4ffc0abf19183e9b865d7fcc78e4
Rennan Melo <rennan.melo11@gmail.com>
2021-07-18 09:32:33 -0300

    Created Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./index.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__title">
+        <span>Qatar 2022</span>
+      </div>
+      <div className="footer__copyright">
+        <span>All rights reserved</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
