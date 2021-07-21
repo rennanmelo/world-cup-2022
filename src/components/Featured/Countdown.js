79c5a5b6ba5761542b2b129d97fb071d639bf040
Rennan Melo <rennan.melo11@gmail.com>
2021-07-21 05:15:49 -0300

    Added slide animation on the Countdown component

--- src/components/Featured/Countdown.js
+++ src/components/Featured/Countdown.js
@@ -1,28 +1,31 @@
 import React from "react";
+import { Slide } from "react-awesome-reveal";
 
 const Countdown = () => {
   return (
-    <div className="countdown">
-      <div className="countdown__top">Event starts in</div>
-      <div className="countdown__bottom">
-        <div className="countdown__item">
-          <div className="countdown__time">27</div>
-          <div className="countdown__tag">days</div>
-        </div>
-        <div className="countdown__item">
-          <div className="countdown__time">27</div>
-          <div className="countdown__tag">hs</div>
-        </div>
-        <div className="countdown__item">
-          <div className="countdown__time">27</div>
-          <div className="countdown__tag">min</div>
-        </div>
-        <div className="countdown__item">
-          <div className="countdown__time">27</div>
-          <div className="countdown__tag">sec</div>
+    <Slide>
+      <div className="countdown">
+        <div className="countdown__top">Event starts in</div>
+        <div className="countdown__bottom">
+          <div className="countdown__item">
+            <div className="countdown__time">27</div>
+            <div className="countdown__tag">days</div>
+          </div>
+          <div className="countdown__item">
+            <div className="countdown__time">27</div>
+            <div className="countdown__tag">hs</div>
+          </div>
+          <div className="countdown__item">
+            <div className="countdown__time">27</div>
+            <div className="countdown__tag">min</div>
+          </div>
+          <div className="countdown__item">
+            <div className="countdown__time">27</div>
+            <div className="countdown__tag">sec</div>
+          </div>
         </div>
       </div>
-    </div>
+    </Slide>
   );
 };
 
