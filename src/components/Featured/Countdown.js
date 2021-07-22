9af35c3f3a0d1295b411e2e350950dee7a49f780
Rennan Melo <rennan.melo11@gmail.com>
2021-07-22 04:46:48 -0300

    Created the dinamic countdown

--- src/components/Featured/Countdown.js
+++ src/components/Featured/Countdown.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Slide } from "react-awesome-reveal";
 
 const Countdown = () => {
@@ -8,6 +8,7 @@ const Countdown = () => {
     minutes: "0",
     seconds: "0",
   });
+  const [willHappen, setWillHappen] = useState(false);
 
   const renderItem = (time, tag) => (
     <div className="countdown__item">
@@ -15,18 +16,45 @@ const Countdown = () => {
       <div className="countdown__tag">{tag}</div>
     </div>
   );
+
+  const getCountdown = useCallback((deadline) => {
+    const time = Date.parse(deadline) - Date.parse(new Date());
+    if (time >= 0) {
+      setWillHappen(true);
+      const seconds = Math.floor((time / 1000) % 60);
+      const minutes = Math.floor((time / 1000 / 60) % 60);
+      const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+      const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+      setTime({
+        seconds,
+        minutes,
+        hours,
+        days,
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    setInterval(() => getCountdown("Nov, 21, 2022, 00:00:00"), 1000);
+  }, [getCountdown]);
+
   return (
-    <Slide>
-      <div className="countdown">
-        <div className="countdown__top">Event starts in</div>
-        <div className="countdown__bottom">
-          {renderItem(time.days, "days")}
-          {renderItem(time.hours, "hs")}
-          {renderItem(time.minutes, "min")}
-          {renderItem(time.seconds, "sec")}
-        </div>
-      </div>
-    </Slide>
+    <>
+      {willHappen && (
+        <Slide>
+          <div className="countdown">
+            <div className="countdown__top">Event starts in</div>
+            <div className="countdown__bottom">
+              {renderItem(time.days, "days")}
+              {renderItem(time.hours, "hs")}
+              {renderItem(time.minutes, "min")}
+              {renderItem(time.seconds, "sec")}
+            </div>
+          </div>
+        </Slide>
+      )}
+    </>
   );
 };
 
