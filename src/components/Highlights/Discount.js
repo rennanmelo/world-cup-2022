e413e9e9fcbfc8c5e1323def17e9c6368eff90be
Rennan Melo <rennan.melo11@gmail.com>
2021-07-24 06:14:56 -0300

    Created html of the Discount component

--- src/components/Highlights/Discount.js
+++ src/components/Highlights/Discount.js
@@ -2,9 +2,21 @@ import React from "react";
 
 const Discount = () => {
   return (
-    <>
-      <div>Discount</div>
-    </>
+    <div className="discount">
+      <div className="discount__content">
+        <div className="discount__percentage">
+          <span className="discount__value">30%</span>
+          <span className="discount__label">off</span>
+        </div>
+        <div className="discount__description">
+          <h3 className="discount__title">Purchase ticket before 20th July</h3>
+          <p className="discount__text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+            pretium blandit est accumsan porttitor. Donec a ornare massa.
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
