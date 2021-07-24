814e2961a68c002a0b298e0bb26904a01f296ed0
Rennan Melo <rennan.melo11@gmail.com>
2021-07-24 06:33:15 -0300

    Added upPercentage animation

--- src/components/Highlights/Discount.js
+++ src/components/Highlights/Discount.js
@@ -1,14 +1,38 @@
-import React from "react";
-import { Slide } from "react-awesome-reveal";
+import React, { useEffect, useState } from "react";
+import { Fade, Slide } from "react-awesome-reveal";
 
 const Discount = () => {
+  const totalPercentage = 30;
+  const [percentage, setPercentage] = useState(0);
+
+  const upPercentage = () => {
+    setPercentage((prevCount) => prevCount + 1);
+  };
+
+  useEffect(() => {
+    if (percentage > 0 && percentage < totalPercentage) {
+      setTimeout(() => {
+        upPercentage();
+      }, 30);
+    }
+  }, [percentage]);
+
   return (
     <div className="discount">
       <div className="discount__content">
-        <div className="discount__percentage">
-          <span className="discount__value">30%</span>
-          <span className="discount__label">off</span>
-        </div>
+        <Fade
+          triggerOnce
+          onVisibilityChange={(inView) => {
+            if (inView) {
+              upPercentage();
+            }
+          }}
+        >
+          <div className="discount__percentage">
+            <span className="discount__value">{percentage}%</span>
+            <span className="discount__label">off</span>
+          </div>
+        </Fade>
         <Slide direction="right">
           <div className="discount__description">
             <h3 className="discount__title">
