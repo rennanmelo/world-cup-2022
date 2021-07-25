b1136d3bab9af68755236f60020a5f7a74853f20
Rennan Melo <rennan.melo11@gmail.com>
2021-07-25 08:01:21 -0300

    Loop through the pricing items

--- src/components/Pricing/index.js
+++ src/components/Pricing/index.js
@@ -2,34 +2,37 @@ import React from "react";
 import "./index.css";
 
 const Pricing = () => {
+  const pricingItems = [
+    {
+      value: 100,
+      package: "balcony",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tristique, lorem nec malesuada aliquam.",
+    },
+    {
+      value: 150,
+      package: "medium",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tristique, lorem nec malesuada aliquam, aslco soplasds quedim.",
+    },
+    {
+      value: 300,
+      package: "star",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tristique, lorem nec.",
+    },
+  ];
   return (
     <div className="pricing">
       <h2 className="pricing__title">Pricing</h2>
       <div className="pricing__content">
-        <div className="pricing__item">
-          <span className="pricing__value">$100</span>
-          <span className="pricing__package">balcony</span>
-          <p className="pricing__description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            tristique, lorem nec malesuada aliquam.
-          </p>
-        </div>
-        <div className="pricing__item">
-          <span className="pricing__value">$150</span>
-          <span className="pricing__package">medium</span>
-          <p className="pricing__description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            tristique, lorem nec malesuada aliquam, aslco soplasds quedim.
-          </p>
-        </div>
-        <div className="pricing__item">
-          <span className="pricing__value">$300</span>
-          <span className="pricing__package">star</span>
-          <p className="pricing__description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            tristique, lorem nec.
-          </p>
-        </div>
+        {pricingItems.map((item) => (
+          <div className="pricing__item" key={item.package}>
+            <span className="pricing__value">${item.value}</span>
+            <span className="pricing__package">{item.package}</span>
+            <p className="pricing__description">{item.description}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
