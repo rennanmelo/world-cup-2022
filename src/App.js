4cbd2476563b5940d760c988fc4977041f9eb86a
Rennan Melo <rennan.melo11@gmail.com>
2021-07-26 05:48:25 -0300

    Added Element tag from the react-scroll library

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Element } from "react-scroll";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Featured from "./components/Featured";
@@ -11,21 +13,21 @@ const App = () => {
   return (
     <div className="App">
       <Header />
-      <>
+      <Element name="featured">
         <Featured />
-      </>
-      <>
+      </Element>
+      <Element name="eventinfo">
         <EventInfo />
-      </>
-      <>
+      </Element>
+      <Element name="highlights">
         <Highlights />
-      </>
-      <>
+      </Element>
+      <Element name="pricing">
         <Pricing />
-      </>
-      <>
+      </Element>
+      <Element name="location">
         <Location />
-      </>
+      </Element>
       <Footer />
     </div>
   );
